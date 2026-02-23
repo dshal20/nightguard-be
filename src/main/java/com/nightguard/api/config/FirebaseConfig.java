41bfbf2978c34e82f57ad75f5c9d121249b8bb20
Ethan Smith <esmith@ethanskills.com>
2026-02-23 08:44:24 -0500

    add firebase config

---
+++ src/main/java/com/nightguard/api/config/FirebaseConfig.java
@@ -0,0 +1,28 @@
+package com.nightguard.api.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+@Configuration
+public class FirebaseConfig {
+  @Bean
+  public FirebaseApp firebaseApp() throws IOException {
+    if (FirebaseApp.getApps().isEmpty()) {
+      FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .build();
+
+      return FirebaseApp.initializeApp(options);
+    }
+    return FirebaseApp.getInstance();
+  }
+}
