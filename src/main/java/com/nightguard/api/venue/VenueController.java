a191ad20cc7dfc0e71e7fd7cae213510c8d245aa
Ethan Smith <esmith@ethanskills.com>
2026-04-05 17:13:30 -0400

    Add endpoint for finding nearby venues

--- src/main/java/com/nightguard/api/venue/VenueController.java
+++ src/main/java/com/nightguard/api/venue/VenueController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,9 @@ public class VenueController {
   }
 
   @GetMapping("/nearby")
-  public ResponseEntity<List<Venue>> getNearbyVenues() {
+  public ResponseEntity<List<Venue>> getNearbyVenues(
+      @RequestParam double latitude,
+      @RequestParam double longitude) {
     return ResponseEntity.ok(venueService.getNearbyVenues());
   }
 
