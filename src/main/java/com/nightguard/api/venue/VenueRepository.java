32a27503962305362d05267f1138bf3243120414
Ethan Smith <esmith@ethanskills.com>
2026-04-13 15:21:47 -0400

    fix sort

--- src/main/java/com/nightguard/api/venue/VenueRepository.java
+++ src/main/java/com/nightguard/api/venue/VenueRepository.java
@@ -3,10 +3,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VenueRepository extends JpaRepository<Venue, UUID> {
   Optional<Venue> findByInviteCode(String inviteCode);
+
+  List<Venue> findAllByOrderByCreatedAtAscIdAsc();
+
+  List<Venue> findByIdInOrderByCreatedAtAscIdAsc(Collection<UUID> ids);
 
   List<Venue> findByDataSharingEnabled(boolean dataSharingEnabled);
 
